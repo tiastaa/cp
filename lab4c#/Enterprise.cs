e53f3abd643b50b0b8e3096b95dad78934fa0155
Nika Yovbak <93946871+tiastaa@users.noreply.github.com>
2022-11-07 00:00:35 +0200

    Create Enterprise.cs

---
+++ lab4c#/Enterprise.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Enterprise
+    {
+        public int id { get; set; }
+        public string enterpriseName { get; set; }
+        public int employees { get; set; }
+        public string productName { get; set; }
+        public string country { get; set; }
+
+
+        public Enterprise(int id, string enterpriseName, int employees, string productName, string country)
+        {
+            this.id = id;
+            this.enterpriseName = enterpriseName;
+            this.employees = employees;
+            this.productName = productName;
+            this.country = country;
+        
+        }
+
+        public Enterprise( string enterpriseName, int employees, string productName, string country)
+        {
+
+            this.id = 0;
+            this.enterpriseName = enterpriseName;
+            this.employees = employees;
+            this.productName = productName;
+            this.country = country;
+        }
+        public Enterprise() { }
+
+        public override string ToString()
+        {
+            return $"Author:{enterpriseName}  with id {id}";
+        }
+    }
+}
